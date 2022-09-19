a8c76f9a82a50a68de1984e7359aa1c991cc04d3
bhaskarkh <bhaskarkh01@gmail.com>
2022-09-19 14:12:59 +0530

    complete program

--- tests/SolMessage.ts
+++ tests/SolMessage.ts
@@ -1,16 +1,44 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { SolMessage } from "../target/types/sol_message";
 
-describe("SolMessage", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
-  const program = anchor.workspace.SolMessage as Program<SolMessage>;
+describe("Testing our messaging app: ", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.SolMessage;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("An account is initialized: ", async function() {
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.rpc.initialize("My first message", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("Data: ", account.data);
+    assert.ok(account.data === "My first message");
+    _baseAccount = baseAccount;
+  });
+
+  it("Update the account previously created: ", async function() {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.update("My second message", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("Updated data: ", account.data);
+    assert.ok(account.data === "My second message");
+    console.log("All account data: ", account);
+    console.log("All data: ", account.dataList);
+    assert.ok(account.dataList.length === 2);
   });
 });
